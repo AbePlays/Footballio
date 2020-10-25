61e4e84337eb29821937e999efd8dab1dbef5298
Abe <abhi.rawat456@gmail.com>
2020-10-25 19:43:00 +0530

    Styled competetion tile

--- src/components/Competetions.vue
+++ src/components/Competetions.vue
@@ -1,15 +1,37 @@
 <template>
-  <div></div>
+  <section>
+    <card
+      v-for="competetion in competetions"
+      :key="competetion.id"
+      :imageUrl="competetion.logo"
+      :name="competetion.name"
+    ></card>
+  </section>
 </template>
 
 <script>
+import Card from "./ui/CompetetionTile.vue";
 export default {
+  components: {
+    Card
+  },
   props: ["code"],
-  mounted() {
-    console.log(this.code);
+  computed: {
+    competetions() {
+      return this.$store.getters.competetions.filter(
+        competetion => competetion.code === this.code
+      );
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+section {
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 80rem;
+  margin: 0 auto;
+  justify-content: center;
+}
 </style>
--- src/components/ui/CompetetionTile.vue
+++ src/components/ui/CompetetionTile.vue
@@ -1,37 +1,44 @@
 <template>
   <div class="card">
-    <img :src="imageUrl" alt="logo" />
-    <div class="card-details">
-      <h1>Country</h1>
-      <p>{{ country }}</p>
-    </div>
+    <img :src="imageUrl" alt="country-flag" />
+    <h1>{{ name }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["imageUrl", "country"]
+  props: ["imageUrl", "name"]
 };
 </script>
 
 <style lang="scss" scoped>
 .card {
   width: 15rem;
-  box-shadow: 0 0 2px black;
+  box-shadow: 20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff;
   border-radius: 5px;
-  margin: 1rem;
+  margin: 3rem 5rem;
+  cursor: pointer;
+  padding: 1rem;
+  overflow: hidden;
 
-  img {
-    width: 100%;
-    border-radius: 5px 5px 0 0;
+  &:hover {
+    img {
+      transform: scale(1.1);
+    }
   }
 
-  &-details {
-    padding: 1rem;
+  img {
+    display: block;
+    margin: 0 auto;
+    border-radius: 5px 5px 0 0;
+    transition: all 500ms ease-in-out;
+  }
 
-    h1 {
-      font-size: 1.5rem;
-    }
+  h1 {
+    font-size: 1.5rem;
+    padding: 1rem;
+    padding-bottom: 0;
+    text-align: center;
   }
 }
 </style>
