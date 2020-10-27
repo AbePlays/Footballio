793a9b90e9b133b66b95af42b209f657ab7c71e0
Abe <abhi.rawat456@gmail.com>
2020-10-27 18:03:37 +0530

    Added Tabs

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,18 +1,26 @@
 <template>
   <section v-if="!isLoading">
-    <div class="image">
-      <img :src="header.imageUrl" alt="logo" />
-    </div>
-    <div class="data">
-      <p>COMPETITION</p>
-      <h1>{{ header.name }}</h1>
+    <div class="header">
+      <div class="image">
+        <img :src="header.imageUrl" alt="logo" />
+      </div>
+      <div class="data">
+        <p>COMPETITION</p>
+        <h1>{{ header.name }}</h1>
+      </div>
     </div>
   </section>
-  <h1 v-else></h1>
+  <h1 v-else>Loading...</h1>
+  <tabs></tabs>
 </template>
 
 <script>
+import Tabs from "./ui/Tabs.vue";
+
 export default {
+  components: {
+    Tabs
+  },
   props: ["id"],
   data() {
     return {
@@ -22,22 +30,22 @@ export default {
       },
       isLoading: true
     };
-  },
-  async updated() {
-    const leagues = this.$store.getters.competitions;
-    leagues.forEach(league => {
-      if (league.id === Number(this.id)) {
-        this.header.imageUrl = league.logo;
-        this.header.name = league.name;
-      }
-    });
-    this.isLoading = false;
   }
+  // async updated() {
+  //   const leagues = this.$store.getters.competitions;
+  //   leagues.forEach(league => {
+  //     if (league.id === Number(this.id)) {
+  //       this.header.imageUrl = league.logo;
+  //       this.header.name = league.name;
+  //     }
+  //   });
+  //   this.isLoading = false;
+  // }
 };
 </script>
 
 <style lang="scss" scoped>
-section {
+.header {
   width: 90%;
   margin: 2rem auto;
   display: flex;
---
+++ src/components/ui/Tabs.vue
@@ -0,0 +1,68 @@
+<template>
+  <section>
+    <div class="tabs">
+      <button @click.prevent="clickHandler" id="news" class="active">
+        News
+      </button>
+      <button @click="clickHandler" id="fixtures">Fixtures</button>
+      <button @click="clickHandler" id="table">Table</button>
+      <button @click="clickHandler" id="goals">Top Scorers</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    clickHandler(event) {
+      this.clearClasses();
+      event.target.classList.add("active");
+    },
+    clearClasses() {
+      const buttons = document.querySelectorAll("button");
+      buttons.forEach(button => {
+        if (button.classList.contains("active")) {
+          button.classList.remove("active");
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.tabs {
+  button {
+    border: none;
+    background-color: white;
+    position: relative;
+    cursor: pointer;
+    font-weight: bold;
+    font-size: 1.15rem;
+
+    &:not(:last-child) {
+      margin-right: 2rem;
+    }
+
+    &::after {
+      content: "";
+      display: none;
+      position: absolute;
+      bottom: -0.5rem;
+      left: 0;
+      width: 100%;
+      height: 0.12rem;
+      border-radius: 5px;
+      background-color: yellowgreen;
+    }
+  }
+
+  .active {
+    color: yellowgreen;
+
+    &::after {
+      display: block;
+    }
+  }
+}
+</style>
